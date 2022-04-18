847a9b4135ff54a87f6914a9fc390d9db3b8891e
klsh99 <faye_012@163.com>
2022-04-18 11:46:53 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git commit -m "commit at: $(date "+%Y-%m-%d %H:%M:%S")"
 git push -u src master
 echo "------------------------- 已将 vuepress 源码提交到 Git ------------------------------------"
 
-yarn docs:build
+npm run docs:build
 cd /home/echoxu/Programe/vuepress/docs/.vuepress/dist
 git init
 git remote add origin git@192.168.1.20:/home/git/blogdata/blog.git
